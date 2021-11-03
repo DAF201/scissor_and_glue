968f493314d8971556e54ce51e2d30875f67edba
DAF201 <56328675+DAF201@users.noreply.github.com>
2021-11-03 18:29:14 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+import setuptools
+
+
+setuptools.setup(
+    name='scissor_and_glue',
+    version='1.0.0',
+    author='DAF201',
+    description='scissor_and_glue',
+    url='https://github.com/DAF201/scissor_and_glue',
+    packages=['scissor_and_glue'],
+    install_requires=[
+    'PIL>=1.1.7',
+    'numpy>=1.16.6',
+    ]
+)
